0eea99a26702f7c7dd781db6bd4c39c0af62ba84
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-26 11:36:21 +0530

    Added: Create a Binary Search Tree

---
+++ BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinaryTree<T> LeftTree { get; set; }
+        public BinaryTree<T> RightTree { get; set; }
+        public BinaryTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.RightTree = null;
+            this.LeftTree = null;
+        }
+       
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinaryTree<T>(item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinaryTree<T>(item);
+                }
+                else
+                {
+                    this.RightTree.Insert(item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.RightTree.Display();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@
         {
             Console.WriteLine("Welcome to the Address Book System");
 
+            BinaryTree<int> bst = new BinaryTree<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Display();
         }
 
     }      
